d2771d6031d8099f4f3f44977becd178ab552694
SemaJ Solomon <sxviersolomon@gmail.com>
2021-09-02 15:09:31 -0400

    inital commit

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Main.css';
+
+export default class Main extends Component {
+
+    render(){
+        return(
+            <div>Main</div>
+        )
+    }
+}
